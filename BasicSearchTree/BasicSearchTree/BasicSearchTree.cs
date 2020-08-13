0f1ec29a383387acf626657db3cfe3d833472641
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-13 19:57:43 +0300

    Started implementing the BinaryTree functions

---
+++ BasicSearchTree/BasicSearchTree/BasicSearchTree.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Xml.Schema;
+
+namespace BinarySearchTree
+{
+    public class BinarySearchTree
+    {
+        public BSTNode Root { get; set; }
+
+        public BinarySearchTree()
+        {
+            Root = null;
+        }
+
+        public BinarySearchTree(int value)
+        {
+            Root = new BSTNode(value);
+        }
+
+        public BinarySearchTree(BSTNode root)
+        {
+            Root = root;
+        }
+
+        public int GetMax()
+        {
+            if(Root == null)
+            {
+                return -1;
+            }
+            return Root.Max();
+        }
+
+        public int GetMin()
+        {
+            if (Root == null)
+            {
+                return -1;
+            }
+            return Root.Min();
+        }
+
+        public bool AddItem(int value)
+        {
+            BSTNode newNode = new BSTNode(value);
+            BSTNode tempParent;
+            var currentNode = Root;
+            if(Root == null)
+            {
+                Root = newNode;
+                return true;
+            }
+            while (true)
+            {
+                tempParent = currentNode;
+                if (value < Root.Value)
+                {
+                    currentNode = currentNode.Left;
+                    if(currentNode == null)
+                    {
+                        tempParent.Left = newNode;
+                        return true;
+                    }
+                }
+                else if(value > Root.Value)
+                {
+                    currentNode = currentNode.Right;
+                    if(currentNode == null)
+                    {
+                        tempParent.Right = newNode;
+                        return true;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public bool Contains(int value)
+        {
+            return FindItem(value) != null;
+        }
+
+        public BSTNode FindItem(int value)
+        {
+            if (Root == null)
+            {
+                return null;
+            }
+            return Root.FindItem(value);
+        }
+
+        public bool DeleteItem(int value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OrderBst()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private BSTNode SortedArrayToBST(int[] array, int start, int end)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private IEnumerable<int> Scan(BSTNode node)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ BasicSearchTree/BasicSearchTree/BasicSearchTreeNode.cs
@@ -0,0 +1,85 @@
+﻿
+namespace BinarySearchTree
+{
+    public class BSTNode
+    {
+        public BSTNode(int value)
+        {
+            Value = value;
+        }
+
+        public int Value { get; private set; }
+
+        public BSTNode Left { get; set; }
+
+        public BSTNode Right { get; set; }
+
+        public int Min()
+        {
+            if (Left == null)
+            {
+                return Value;
+            }
+
+            var minVal = Value;
+            while (Left != null)
+            {
+                minVal = Left.Value;
+            }
+
+            return minVal;
+        }
+
+        public int Max()
+        {
+            if (Right == null)
+            {
+                return Value;
+            }
+            var maxVal = Value;
+            while (Right != null)
+            {
+                maxVal = Right.Value;
+            }
+
+            return maxVal;
+        }
+
+        public bool AddItem(int value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Contains(int value)
+        {
+            return FindItem(value) != null;
+        }
+
+        public BSTNode FindItem(int value)
+        {
+            if(Value == value)
+            {
+                return this;
+            }
+            if(Left == null && Right == null)
+            {
+                return null;
+            }
+            var lRes = FindItem(Left.Value);
+            var rRes = FindItem(Right.Value);
+            if(lRes != null)
+            {
+                return lRes;
+            }
+            else
+            {
+                return rRes;
+            }
+        }
+
+        public bool DeleteItem(int value, BSTNode parent)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
